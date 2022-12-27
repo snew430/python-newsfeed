02eadb00420fabcb8d8df19db26939b275be626c
snew430 <seanpnew@hotmail.com>
2022-12-27 16:15:31 -0500

    added post model

--- seeds.py
+++ seeds.py
@@ -1,4 +1,4 @@
-from app.models import User
+from app.models import User, Post
 from app.db import Session, Base, engine
 
 Base.metadata.drop_all(engine)
@@ -13,6 +13,16 @@ db.add_all([
     User(username='sean', email='sean@sean.com', password='pass1234'),
     User(username='kerry', email='kerry@kerry.com', password='pass1234')
     ])
+
+db.commit()
+# insert posts
+db.add_all([
+  Post(title='Donec posuere metus vitae ipsum', post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
+  Post(title='Morbi non quam nec dui luctus rutrum', post_url='https://nasa.gov/donec.json', user_id=1),
+  Post(title='Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue', post_url='https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx', user_id=2),
+  Post(title='Nunc purus', post_url='http://desdev.cn/enim/blandit/mi.jpg', user_id=3),
+  Post(title='Pellentesque eget nunc', post_url='http://google.ca/nam/nulla/integer.aspx', user_id=4)
+])
 
 db.commit()
 
