9ca1b7e22594ca0ac2f5fa8361da125b49d74d36
snew430 <seanpnew@hotmail.com>
2023-01-05 14:35:32 -0500

    added filters and updated html

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,25 @@
 from flask import Flask
 from app.routes import home
 from app.routes import dashboard
+from app.db import init_db
+from app.utils import filters
+
 
 def create_app(test_config=None):
     # set up app config
-    app = Flask(__name__,static_url_path="/")
+    app = Flask(__name__, static_url_path="/")
     app.url_map.strict_slashes = False
     app.config.from_mapping(
-        SECRET_KEY = 'super_secret_key'
+        SECRET_KEY='super_secret_key'
     )
-
-    @app.route('/hello')
-    def hello():
-        return "hello world"
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
 
     # register routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+
+    init_db(app)
 
     return app
